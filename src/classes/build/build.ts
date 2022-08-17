3a350e4db80f0508ed9523117e04ab7e98fa385a
Dawid Lubowiecki <lubowiecki@gmail.com>
2022-08-17 10:38:51 +0200

    v1.0.0

---
+++ src/classes/build/build.ts
@@ -0,0 +1,23 @@
+import gulp from 'gulp';
+import { Notify } from '@opi-pib/node-utility';
+import { isDefined } from '@opi-pib/ts-utility';
+
+import { Loader } from '../loader/loader';
+import { Config } from '../../config/models/config';
+import { Core } from '../core/core';
+
+export class Build {
+	static emails(configPath?: string): void {
+		const config: Config = Loader.loadConfig(configPath);
+		const core: Core = new Core(config, true);
+		core.setTasks();
+
+		gulp.series(['build'])((error?: any) => {
+			if (isDefined(error)) {
+				Notify.error({ error });
+			} else {
+				Notify.success({ message: 'Done' });
+			}
+		});
+	}
+}
---
+++ src/cmds/serve.ts
@@ -0,0 +1,6 @@
+import { Serve } from '../classes/serve/serve';
+
+exports.command = 'serve [config]';
+exports.aliases = ['s'];
+exports.desc = 'Serve emails';
+exports.handler = (argv: any) => Serve.emails(argv.config);
